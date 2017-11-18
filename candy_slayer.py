467cfad2495e1c9cb1b58514d3c39c0ef0b9fd78
Kasey Ann <kaseystowell@Kaseys-MacBook-Pro.local>
2017-11-18 15:37:21 -0500

    main title added

--- candy_slayer.py
+++ candy_slayer.py
@@ -1,12 +1,16 @@
 import sys
 import pygame
+import os
 from candy_slayer.Game import Game
 
 def run_game():
     # Initalize game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode((600, 540))
     pygame.display.set_caption("Candy Slayer")
+    bg = pygame.image.load(os.path.join("candy_slayer/assets", "title.png")).convert()
+    bg = pygame.transform.scale(bg, (600, 540))
+    screen.blit(bg, (0, 0))
 
     # Create the game.
     game = Game()
@@ -17,7 +21,7 @@ def run_game():
         # Watch for keyboard and mouse events.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                sys.exit()
+               sys.exit()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
